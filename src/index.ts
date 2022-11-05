063da93a9c3de493edffefc44e8cbabb2ba87f02
dsva <darwin97.va@gmail.com>
2022-11-04 21:39:07 -0500

    4

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,15 @@ import { apiRouter } from "./api";
 
 const app = express();
 
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", apiRouter);
 
-app.use(cors());
+app.get("/", (_req, res) => res.send("Hi there!"));
 
 const httpServer = createHttpServer(app);
 

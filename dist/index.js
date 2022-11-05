20e09e2f90efded3df86d7194b0febdac473944c
dsva <darwin97.va@gmail.com>
2022-11-04 21:45:57 -0500

    5

--- dist/index.js
+++ dist/index.js
@@ -22078,6 +22078,155 @@ var require_lib3 = __commonJS({
   }
 });
 
+// node_modules/.pnpm/dotenv@16.0.3/node_modules/dotenv/package.json
+var require_package = __commonJS({
+  "node_modules/.pnpm/dotenv@16.0.3/node_modules/dotenv/package.json"(exports, module2) {
+    module2.exports = {
+      name: "dotenv",
+      version: "16.0.3",
+      description: "Loads environment variables from .env file",
+      main: "lib/main.js",
+      types: "lib/main.d.ts",
+      exports: {
+        ".": {
+          require: "./lib/main.js",
+          types: "./lib/main.d.ts",
+          default: "./lib/main.js"
+        },
+        "./config": "./config.js",
+        "./config.js": "./config.js",
+        "./lib/env-options": "./lib/env-options.js",
+        "./lib/env-options.js": "./lib/env-options.js",
+        "./lib/cli-options": "./lib/cli-options.js",
+        "./lib/cli-options.js": "./lib/cli-options.js",
+        "./package.json": "./package.json"
+      },
+      scripts: {
+        "dts-check": "tsc --project tests/types/tsconfig.json",
+        lint: "standard",
+        "lint-readme": "standard-markdown",
+        pretest: "npm run lint && npm run dts-check",
+        test: "tap tests/*.js --100 -Rspec",
+        prerelease: "npm test",
+        release: "standard-version"
+      },
+      repository: {
+        type: "git",
+        url: "git://github.com/motdotla/dotenv.git"
+      },
+      keywords: [
+        "dotenv",
+        "env",
+        ".env",
+        "environment",
+        "variables",
+        "config",
+        "settings"
+      ],
+      readmeFilename: "README.md",
+      license: "BSD-2-Clause",
+      devDependencies: {
+        "@types/node": "^17.0.9",
+        decache: "^4.6.1",
+        dtslint: "^3.7.0",
+        sinon: "^12.0.1",
+        standard: "^16.0.4",
+        "standard-markdown": "^7.1.0",
+        "standard-version": "^9.3.2",
+        tap: "^15.1.6",
+        tar: "^6.1.11",
+        typescript: "^4.5.4"
+      },
+      engines: {
+        node: ">=12"
+      }
+    };
+  }
+});
+
+// node_modules/.pnpm/dotenv@16.0.3/node_modules/dotenv/lib/main.js
+var require_main = __commonJS({
+  "node_modules/.pnpm/dotenv@16.0.3/node_modules/dotenv/lib/main.js"(exports, module2) {
+    var fs = require("fs");
+    var path = require("path");
+    var os = require("os");
+    var packageJson = require_package();
+    var version = packageJson.version;
+    var LINE = /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/mg;
+    function parse(src) {
+      const obj = {};
+      let lines = src.toString();
+      lines = lines.replace(/\r\n?/mg, "\n");
+      let match;
+      while ((match = LINE.exec(lines)) != null) {
+        const key = match[1];
+        let value = match[2] || "";
+        value = value.trim();
+        const maybeQuote = value[0];
+        value = value.replace(/^(['"`])([\s\S]*)\1$/mg, "$2");
+        if (maybeQuote === '"') {
+          value = value.replace(/\\n/g, "\n");
+          value = value.replace(/\\r/g, "\r");
+        }
+        obj[key] = value;
+      }
+      return obj;
+    }
+    function _log(message) {
+      console.log(`[dotenv@${version}][DEBUG] ${message}`);
+    }
+    function _resolveHome(envPath) {
+      return envPath[0] === "~" ? path.join(os.homedir(), envPath.slice(1)) : envPath;
+    }
+    function config(options) {
+      let dotenvPath = path.resolve(process.cwd(), ".env");
+      let encoding = "utf8";
+      const debug = Boolean(options && options.debug);
+      const override = Boolean(options && options.override);
+      if (options) {
+        if (options.path != null) {
+          dotenvPath = _resolveHome(options.path);
+        }
+        if (options.encoding != null) {
+          encoding = options.encoding;
+        }
+      }
+      try {
+        const parsed = DotenvModule.parse(fs.readFileSync(dotenvPath, { encoding }));
+        Object.keys(parsed).forEach(function(key) {
+          if (!Object.prototype.hasOwnProperty.call(process.env, key)) {
+            process.env[key] = parsed[key];
+          } else {
+            if (override === true) {
+              process.env[key] = parsed[key];
+            }
+            if (debug) {
+              if (override === true) {
+                _log(`"${key}" is already defined in \`process.env\` and WAS overwritten`);
+              } else {
+                _log(`"${key}" is already defined in \`process.env\` and was NOT overwritten`);
+              }
+            }
+          }
+        });
+        return { parsed };
+      } catch (e) {
+        if (debug) {
+          _log(`Failed to load ${dotenvPath} ${e.message}`);
+        }
+        return { error: e };
+      }
+    }
+    var DotenvModule = {
+      config,
+      parse
+    };
+    module2.exports.config = DotenvModule.config;
+    module2.exports.parse = DotenvModule.parse;
+    module2.exports = DotenvModule;
+  }
+});
+
 // node_modules/.pnpm/base64id@2.0.0/node_modules/base64id/lib/base64id.js
 var require_base64id = __commonJS({
   "node_modules/.pnpm/base64id@2.0.0/node_modules/base64id/lib/base64id.js"(exports, module2) {
@@ -30070,7 +30219,7 @@ var require_uws = __commonJS({
 });
 
 // node_modules/.pnpm/socket.io@4.5.3/node_modules/socket.io/package.json
-var require_package = __commonJS({
+var require_package2 = __commonJS({
   "node_modules/.pnpm/socket.io@4.5.3/node_modules/socket.io/package.json"(exports, module2) {
     module2.exports = {
       name: "socket.io",
@@ -30236,7 +30385,7 @@ var require_dist2 = __commonJS({
     var typed_events_1 = require_typed_events();
     var uws_1 = require_uws();
     var debug = (0, debug_1.default)("socket.io:server");
-    var clientVersion = require_package().version;
+    var clientVersion = require_package2().version;
     var dotMapRegex = /\.map/;
     var Server2 = class extends typed_events_1.StrictEventEmitter {
       constructor(srv, opts = {}) {
@@ -30573,6 +30722,8 @@ var import_http = require("http");
 var import_cors = __toESM(require_lib3());
 
 // src/config/index.ts
+var import_dotenv = __toESM(require_main());
+import_dotenv.default.config();
 var PORT = Number(process.env.PORT) || 3030;
 
 // node_modules/.pnpm/socket.io@4.5.3/node_modules/socket.io/wrapper.mjs
@@ -30602,8 +30753,11 @@ var apiRouter = router2;
 
 // src/index.ts
 var app = (0, import_express3.default)();
-app.use("/api", apiRouter);
 app.use((0, import_cors.default)());
+app.use(import_express3.default.json());
+app.use(import_express3.default.urlencoded({ extended: true }));
+app.use("/api", apiRouter);
+app.get("/", (_req, res) => res.send("Hi there!"));
 var httpServer = (0, import_http.createServer)(app);
 initWSocket(httpServer);
 httpServer.listen(PORT, () => console.log(`Running in ${PORT} port.`));
